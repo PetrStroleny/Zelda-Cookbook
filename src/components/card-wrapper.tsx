aa7a9cdcd01557d8fab06f0665d35405a485cf19
Petr Strolený <petr.stroleny@gmail.com>
2023-03-01 23:04:39 +0100

    padding fix

--- src/components/card-wrapper.tsx
+++ src/components/card-wrapper.tsx
@@ -3,15 +3,12 @@ import styled from "@emotion/styled";
 const CardWrapper = styled("div")`
     width: 100%;
     display: grid;
-    padding: 40px;
     gap: 20px;
 
     @media only screen and (max-width: ${p => p.theme.breakPoints.mobile}px) {
-        padding: 14px 12px;
         gap: 13px;
     }
 
-    
     grid-template-columns: repeat(auto-fill, minmax(336px, 1fr));
 
     @media only screen and (max-width: ${p => p.theme.breakPoints.mobile}px) {
