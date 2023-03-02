2a0936d1063f1745bbedd3c198135bc871ccaabc
Petr Strolený <petr.stroleny@gmail.com>
2023-03-02 09:40:22 +0100

    a tag update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,38 @@
 import styled from "@emotion/styled";
-import { Link } from "wouter";
+import { Link, useRoute } from "wouter";
 
-const Header = () => (
-    <Wrapper>
-        <Link href="/">
-            Zelda Cookbook
-        </Link>
-        <Link href="/">
-            Ingredience
-        </Link>
-        <Link href="/recepty">
-            Recepty
-        </Link>
-        <Link href="/lokace">
-            Lokace
-        </Link>
-    </Wrapper>
-);
+const Header = () => {
+
+    const [homeActive] = useRoute("/");
+    const [recipesActive] = useRoute("/recepty");
+    const [locationsActive] = useRoute("/lokace");
+
+    return(
+        <Wrapper>
+            <Link href="/">
+                Zelda Cookbook
+            </Link>
+            <Link className={homeActive ? "active" : ""} href="/">
+                Ingredience
+            </Link>
+            <Link className={recipesActive ? "active" : ""} href="/recepty">
+                Recepty
+            </Link>
+            <Link className={locationsActive ? "active" : ""} href="/lokace">
+                Lokace
+            </Link>
+        </Wrapper>
+    );
+}
 
 const Wrapper = styled("header")`
     border: 1px solid red;
     > a {
         margin-right: 10px;
+
+        &.active {
+            color: red;
+        }
     }
 `;
 
